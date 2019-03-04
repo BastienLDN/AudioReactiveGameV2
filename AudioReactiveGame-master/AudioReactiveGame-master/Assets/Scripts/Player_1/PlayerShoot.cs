d1a1db7c07f9b941120e8ab7b52d1ae9ad7ce062
BastienLDN <47496886+BastienLDN@users.noreply.github.com>
2019-03-04 21:12:16 +0100

    Initial commit

---
+++ AudioReactiveGame-master/AudioReactiveGame-master/Assets/Scripts/Player_1/PlayerShoot.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    #region Public members 
+
+    public GameObject m_targetPlayer;
+    public Transform m_pistol;
+    public float m_orbitDistance;
+    public float m_speed;
+    public Vector3 m_orbit;
+
+    #endregion
+
+
+    #region System methods
+
+    private void Awake()
+    {
+        _tr = m_pistol;
+    }
+
+    private void LateUpdate()
+    {
+        Orbit();
+    }
+
+    #endregion
+
+
+    #region Main methods
+
+    private void Orbit()
+    {
+        _tr.RotateAround(m_targetPlayer.transform.position, m_orbit, 100 * Time.deltaTime * m_speed);
+    }
+
+    #endregion 
+
+
+    #region Private and protected members 
+
+    private Transform _tr;
+
+    #endregion
+}
---
+++ AudioReactiveGame-master/AudioReactiveGame-master/Assets/Scripts/Player_2/SecondPlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SecondPlayerMove : MonoBehaviour
+{
+    #region Public members 
+
+    public float m_speed;
+    public Transform m_tr; // Enemy get the player position
+
+    #endregion
+
+
+    #region System methods
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        m_tr = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal_P2");
+        float vertical = Input.GetAxisRaw("Vertical_P2");
+
+        _rb.velocity = new Vector2(horizontal, vertical) * m_speed * Time.deltaTime;
+    }
+
+    #endregion
+
+
+    #region Private and protected members 
+
+    private Rigidbody _rb;
+
+    #endregion
+}
